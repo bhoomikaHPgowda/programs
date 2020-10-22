14a121cce6ad54344b8abeac775fcc910031a3bf
karthik-k4 <71215735+karthik-k4@users.noreply.github.com>
2020-10-22 22:30:50 +0530

    Update recursive.swift
    Lots of alignment issues, some logical issues.

--- recursive.swift
+++ recursive.swift
@@ -1,16 +1,16 @@
-let power(base : Double,power : Double) -> Double
+let power(base : Double,power : Double) -> Double //The opening flower bracket should be in the same line as tha of the function declaration
  {   
-       if power == 0
+       if power == 0 //Too much tab space before the 'if' and flower bracket should begin in the same line as that of if.
       {
            return 1.0
        }
        else
       {
        var result : Double
-       result = pow(base,power)
+       result = pow(base,power) //The task was to calculate power using recursion. If you are directly using the pow function, it is not recursion.
        return(result)
        }  
    }
-   print(power(base : 5,power : 2))
+   print(power(base : 5,power : 2)) //Comma should be followed by a space. There should not be space before :
     
     
